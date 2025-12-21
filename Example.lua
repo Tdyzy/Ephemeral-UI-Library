fcb25393bc8717e0e79d523fc010d22de6371899
Tdyyy <cerauskastadas@gmail.com>
2025-12-21 16:15:43 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,32 +1,36 @@
--- Use example of Ephemeral UI library
-local Ephemeral = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tdyzy/Ephemeral-UI-Library/refs/heads/main/Library.lua"))()
+local Ephemeral = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tdyzy/Tdyyyz-UI-library/refs/heads/main/Library.lua"))()
 
-local Main = Ephemeral:Init(Enum.KeyCode.RightControl)
+-- Define your menu structure here
+local myConfig = {
+	{
+		Title = "Combat",
+		Items = {
+			{Name = "KillAura", Callback = function(s) print("KillAura: ", s) end},
+			{Name = "Trigger Bot", Callback = function(s) print("Trigger: ", s) end},
+			{Name = "Infinite Ammo", Callback = function(s) print("Ammo: ", s) end}
+		}
+	},
+	{
+		Title = "Movement",
+		Items = {
+			{Name = "Speed", Callback = function(s) 
+                game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s and 100 or 16 
+            end},
+			{Name = "Super Jump", Callback = function(s) 
+                game.Players.LocalPlayer.Character.Humanoid.JumpPower = s and 150 or 50 
+            end},
+			{Name = "Noclip", Callback = function(s) print("Noclip: ", s) end}
+		}
+	},
+	{
+		Title = "Settings",
+		Items = {
+			{Name = "Destroy UI", Callback = function() 
+                game.Players.LocalPlayer.PlayerGui.Ephemeral_V2:Destroy() 
+            end}
+		}
+	}
+}
 
--- Local Player
-local PlayerWin = Main:NewWindow("Self", UDim2.new(0.05, 0, 0.2, 0))
-PlayerWin:AddButton("Reset Character", function() 
-    game.Players.LocalPlayer.Character.Humanoid.Health = 0 
-end)
-PlayerWin:AddToggle("God Mode (Mock)", function(s) print(s) end)
-
--- Movement
-local Movement = Main:NewWindow("Movement", UDim2.new(0.2, 0, 0.2, 0))
-Movement:AddSlider("Speed", 16, 250, 16, function(v)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-end)
-Movement:AddSlider("Jump", 50, 500, 50, function(v)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
-end)
-Movement:AddToggle("Spin", function(s) _G.Spin = s 
-    task.spawn(function() while _G.Spin do 
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame *= CFrame.Angles(0,0.5,0) task.wait() 
-    end end)
-end)
-
--- Settings
-local Settings = Main:NewWindow("Settings", UDim2.new(0.35, 0, 0.2, 0))
-Settings:AddKeybind("Menu Key", Enum.KeyCode.RightControl, Main)
-Settings:AddButton("Unload UI", function()
-    game.Players.LocalPlayer.PlayerGui:FindFirstChild("Ephemeral_Lib"):Destroy()
-end)
+-- Initialize everything at once
+Ephemeral:Init(myConfig, Enum.KeyCode.RightControl)
